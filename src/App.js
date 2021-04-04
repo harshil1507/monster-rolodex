63429ab8b8b044ba7fff89398718beaf1bb8c2d0
harshil maniar <maniarharshil1000@gmail.com>
2021-04-04 11:20:10 +0530

    added gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
 import React, { Component } from "react";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -8,8 +9,13 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -18,9 +24,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster} />
       </div>
     );
   }
